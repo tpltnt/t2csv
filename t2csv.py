ca91ed61f438679c896440297d1ed4fd5e8f0fd4
tpltnt <1172976+tpltnt@users.noreply.github.com>
2019-07-16 22:34:01 +0200

    parse and print ratings

--- t2csv.py
+++ t2csv.py
@@ -4,12 +4,43 @@ This script takes the CSV data exported from the t2
 moodtracker and formats it with one rating per column
 for better processing.
 """
-
 import argparse
+import csv
 
+# parse commandline arguments
 PARSER = argparse.ArgumentParser(description='Format t2 CSV for further processing.')
 PARSER.add_argument('t2file', type=str, help='CSV file exported from t2 modd tracker')
 ARGS = PARSER.parse_args()
 
+# scale metadata
+SCALE = ""
+LOW_END = ""
+HIGH_END = ""
+
+NEW_ROWS = []
+# read data from file
 with open(ARGS.t2file, "r") as infile:
-    pass
+    READER = csv.reader(infile, delimiter=',', quotechar='"')
+    for row in READER:
+        if row[0] == 'group':
+            continue
+
+        # catch new rating scale (for the following data)
+        if row[0] == 'scale':
+            SCALE = row[0]
+            LOW_END = row[1]
+            HIGH_END = row[2]
+
+        # actually process the result
+        if row[0] == 'result':
+            timestamp = row[1]
+            rating = row[2]
+            data = dict()
+            data['low'] = LOW_END
+            data['high'] = HIGH_END
+            data['timestamp'] = timestamp
+            data['rating'] = rating
+            NEW_ROWS.append(data)
+        # TODO: process notes
+
+print(NEW_ROWS)
